e3ad67b2b6da8948c8ad261ff5b51b60e953b099
Yashwanth718 <yashwanthkolli01@gmail.com>
2024-07-18 14:59:19 +0530

    commit

---
+++ backend/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require("./routes");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+const app = express();
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "*",
+    // allowedHeaders: ["*"],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  })
+);
+app.options(
+  "/api/v1/user/sigin",
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  })
+);
+app.use(
+  bodyParser({
+    limit: "50mb",
+  })
+);
+async function connetToDB() {
+  await mongoose.connect("mongodb+srv://yashwanthkolli:yashDB123@cluster0.ozocxal.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+}
+
+connetToDB();
+
+app.use("/api/v1", router);
+
+app.listen(3000, () => {
+  console.log("App running on port 3000");
+});
